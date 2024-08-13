b48e0a29fb751adf682ad77bd9a66658eb8af993
jodomodev <joedodaniljr123ggmail.com>
2024-08-13 11:26:21 -0400

    index modifications

--- index.js
+++ index.js
@@ -17,18 +17,20 @@ app.use(cors({
     credentials: true,
 }))
 
-
 app.get('/', async (req, res) => {
-    const token = req.cookies
 
-    if (token == undefined) {
-        res.json({ message: 'Not authenticated' })
-    } else {
-        const data = jwt.verify(token, SECRET_JWT_KEY)
-        res.json({ data })
-    }
+    const token = req.cookies.access_token
 
+    console.log(token)
 
+    return res.json({ hola: 'nose' })
+
+    // if (token) {
+    //     res.json({ conect: false })
+    // } else {
+    //     const data = jwt.verify(token, process.env.SECRET_JWT_KEY)
+    //     res.json({ conect: true, data })
+    // }
 })
 
 app.post('/login', async (req, res) => {
@@ -42,23 +44,37 @@ app.post('/login', async (req, res) => {
         });
 
         if (!user) {
-            return res.status(409).json({ message: 'User not exist' });
+            return res.status(409).json({ isValid: false });
         }
 
 
 
         const token = jwt.sign({ id: user.id, username: user.username }, process.env.SECRET_JWT_KEY, { expiresIn: '1h' })
 
-        console.log(token)
+        // console.log(token)
 
         res
             .cookie('access_token', token, {
-                // httpOnly: true, // solo tienen acceso en el servidor
+                httpOnly: true, // solo tienen acceso en el servidor
                 // secure: process.env.NODE_ENV === 'production',
                 // sameSite: 'strict', // la cookie solo se puede acceder en el mismo dominio
                 maxAge: 1000 * 60 * 60 // la cookie solo tiene validez 1 hora
             })
-            .json({ message: 'loged' })
+            .json({ isValid: true })
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+
+
+})
+
+app.get('/logout', async (req, res) => {
+    try {
+        res
+            .clearCookie('access_token')
+            .json('logout successfull')
+
     } catch (error) {
         console.error('Error registering user:', error);
         res.status(500).json({ message: 'Internal server error' });
@@ -100,5 +116,4 @@ app.post('/register', async (req, res) => {
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on port ${PORT}`)
-    console.log(process.env)
 })
